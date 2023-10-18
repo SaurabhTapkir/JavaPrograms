161cfe9cca24a34b82e2090e70c209395e644410
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-18 22:18:01 +0530

    Create DataHiding_Main.java
    Main Method of DataHiding_1 class

---
+++ Oops/DataHiding_Main.java
@@ -0,0 +1,9 @@
+ //main():
+class DataHiding_Main{
+    public static void main(String[] args)
+    {
+    DataHiding obj= new DataHiding("Saurabh Tapkir",2,"India");
+     // System.out.println(obj.Name);// it will not work
+
+    }
+}
