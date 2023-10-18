d71078d943d87b33b665eb45d0ca34580eb4f3f9
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-18 22:23:39 +0530

    Update DataHiding_1.java
    use getter and setter

--- Oops/DataHiding_1.java
+++ Oops/DataHiding_1.java
@@ -13,6 +13,25 @@ class DataHiding
       this.RollNo=rollno;
       this.Address=addrs;
     }
+
+    // to read use getter()
+   public String get_studname()
+    {
+      return this.Name;
+    }
     
-   
+    //to modify Use Setter()
+   public void set_studname(String n)
+    {
+       this.Name=n;
+    }
+    // non static method for display details
+    public void Details()
+    {
+      System.out.println("Student Name"+this.Name);
+      System.out.println("Student Rollno"+this.Rollno)
+      System.out.println("Student Address"+this.Address);
+      System.out.println(""+this.Name);
   }
+  }
+
