40a8da44918464a6f94046153c8769e766bd1ef6
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-18 21:50:04 +0530

    Create DataHiding_1.java
    Its is Data Hiding_1 class with Non static Member only 
    There is no main() in it

---
+++ Oops/DataHiding_1.java
@@ -0,0 +1,18 @@
+//it constains Non static members
+class DataHiding
+  {
+    //Non static Variable:
+   private  String Name;
+    int RollNo;
+    String Address;
+
+//parameterized Constructor
+    DataHiding(String name,int rollno,String addrs)
+    {
+      this.Name=name;
+      this.RollNo=rollno;
+      this.Address=addrs;
+    }
+    
+   
+  }
