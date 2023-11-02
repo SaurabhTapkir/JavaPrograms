26ec80714fa077f842ef335c633941a04e886c1c
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-11-02 22:29:52 +0530

    Add files via upload

---
+++ String/StringProgram.java
@@ -0,0 +1,25 @@
+ class StringProgram {
+  public static void main(String[] args) {
+    //declare string
+    String fname="saurabh";
+    String lname="Tapkir";
+    
+    System.out.println(fname);
+    
+    //counting the char in str 
+    System.out.println(fname.length());
+    
+    //convertin string to Upper Case:
+    System.out.println(fname.toUpperCase());
+    
+    //Find char in string
+    System.out.println(fname.indexOf('s'));
+    
+    //check str is empty
+    System.out.println(fname.isEmpty());
+    //check whether str is equal to that value or no
+    System.out.println(fname.equals("saurabh"));
+    //char at use to  finds which element at index  
+System.out.println(fname.charAt(0)
+);  }  
+}
