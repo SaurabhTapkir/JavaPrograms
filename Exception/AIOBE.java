1ce0a4ded3210edb2fce2035fd2d05164ba2eca7
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-11-06 20:30:13 +0530

    ArrayIndexOutOfBoundsException

---
+++ Exception/AIOBE.java
@@ -0,0 +1,13 @@
+/*
+ ArrayIndexOutOfBoundsException - occurs when we try
+ to access value from given array based on its index value ,
+ if index value  is greater or equal to length of given array
+  
+ */
+class AIOBE {
+    public static void main(String[] args) {
+        int a[] =new int[4];
+        System.out.println(a[4]);
+    }
+    
+}
