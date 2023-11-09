26c1ea82fd1469d3588810445445c269e0a08fb1
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-11-09 21:33:50 +0530

    Create FNFE.java
    FileNotFoundException

---
+++ Exception/FNFE.java
@@ -0,0 +1,11 @@
+//FileNotFoundException
+class FNFE
+{
+  public static void main(String[] args)
+  {
+    FileOutPutStream f= new FileOutputStream("C://new.txt"); //path to create file
+  }
+}
+
+//but this will create an exception FileNotFoundException so we can use Throw Keyword 
+//Throw used to declare and ignore exception
