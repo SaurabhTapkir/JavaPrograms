14a82110cbf3a2527f3dd5f8028582808ad1b77b
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-11-10 21:09:03 +0530

    Create Login.java

---
+++ Exception/Handling Exception/CustomException/LoginException/Login/Login.java
@@ -0,0 +1,18 @@
+class Login
+  {
+    public static void input(String username)
+    {
+      if(username=="SaurbhTapkir")
+      {
+        System.out.println( username+" Login Successfully...");
+      }
+      else
+      {
+        System.out.println( username+" Login Successfully...");
+      }
+    }
+    public static void main()
+    {
+      input("SaurabhTapkir");
+    }
+  }
