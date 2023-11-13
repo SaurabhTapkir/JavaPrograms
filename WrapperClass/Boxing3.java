a0134e42fc307f18518f3eef8690ee517f554f98
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-11-13 12:58:25 +0530

    using new keyword

---
+++ WrapperClass/Boxing3.java
@@ -0,0 +1,25 @@
+//Boxing using new Keyword but this method is no longer use 
+
+class Boxing3 {
+    public static void main(String[] args) {
+        byte byt= 1;
+Byte b= new Byte(byt);
+System.out.println(byt);
+System.out.println(b);
+System.out.println();
+
+
+int a= 100;
+Integer i= new Integer(a);
+System.out.println(i);
+System.out.println(a);
+System.out.println();
+
+float f= 100;
+Float F= new Float(f);
+System.out.println(f);
+System.out.println(F);
+
+
+    }
+}
