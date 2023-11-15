054ae3ee5b07eac0dcc7c1b3dd02044a01f946d2
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-11-15 22:04:19 +0530

    Unnboxing
    Unnboxing is convert non primitive type values to primitive type val

---
+++ WrapperClass/Unboxing.java
@@ -0,0 +1,21 @@
+/*
+ Unboxing :
+ it is process of Converting Non primitive type value back to its primitive datatype
+there are two ways to perform unboxing
+1.literal way
+2. using value() of wrapper class
+ -----------------------------------------
+                1.literal way
+                ---------------
+*/
+ 
+class Unboxing {
+    public static void main(String[] args) {
+    Integer i=120;
+    int a=i; // passing non primitive to primitive        
+    System.out.println(i.MIN_VALUE); //non primitive
+   System.out.println(a);//a.MAX_VALUE 
+     
+    }
+    
+}
